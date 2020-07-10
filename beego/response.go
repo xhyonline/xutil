5019808e5838d4df6cd41c8f6faa8b5d429a43e4
许弘毅 <383164014@qq.com>
2020-07-11 00:04:48 +0800

    新增工具包

--- beego/response.go
+++ beego/response.go
@@ -1,1 +1,16 @@
 package beego
+
+type respData struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"message"`
+	Data interface{} `json:"data"`
+}
+
+// NewResponse 数据响应格式
+func NewResponse(code int, msg string, data interface{}) *respData {
+	return &respData{
+		Code: code,
+		Msg:  msg,
+		Data: data,
+	}
+}
