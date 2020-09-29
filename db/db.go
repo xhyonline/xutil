442630470bd91f87a5a1b107d494bd494c156391
383164014@qq.com <383164014@qq.com>
2020-09-29 17:45:02 +0800

    新增 mysql 连接池

--- db/db.go
+++ db/db.go
@@ -18,6 +18,12 @@ type Config struct {
 	Password string `default:"root"`
 	Name     string
 	Lifetime int `default:"3000"`
+	// 此连接池和 kv 包中有所不同,连接池中的连接你不需要主动获取和释放
+	// 你仅仅只需要 NewDataBase() 一个实例,直接获取实例即可,当执行 orm 操作时,它内部自动会为你维护连接池。这一切都是无感知的
+	// 你可以通过查看 mysql 终端中的连接数来看到这一现象
+	MaxActiveConn int `default:"100"` // 设置数据库连接池最大连接数
+	MaxIdleConn   int `default:"20"`  // 连接池最大允许的空闲连接数，如果没有sql任务需要执行的连接数大于20，超过的连接会被连接池关闭。
+
 }
 
 // NewDataBase 实例化一个数据库
@@ -34,6 +40,8 @@ func NewDataBase(c *Config) *gorm.DB {
 			continue
 		}
 		db.DB().SetConnMaxLifetime(time.Duration(c.Lifetime) * time.Second)
+		db.DB().SetMaxOpenConns(c.MaxActiveConn)
+		db.DB().SetMaxIdleConns(c.MaxIdleConn)
 		log.Info("Mysql connect successful.")
 		break
 	}
