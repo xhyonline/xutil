24c4c5915b61d032137048d7ea4ed376db3f8568
383164014@qq.com <383164014@qq.com>
2020-12-02 17:03:32 +0800

    持续集成

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/xhyonline/xutil/kv"
-	_ "github.com/ziutek/mymysql/native"
-)
-
 func main() {
 
-	config := &kv.Sentinel{
-		MasterName:   "myMaster",
-		SentinelIP:   "121.36.253.109",
-		SentinelPort: "26379",
-	}
-
-	client, err := kv.SentinelGetClient(config)
-
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(client.SetString("name", "888", 0))
 }
