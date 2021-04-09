1119dc4471c186de1a146a81a8c1ed14482e151a
许弘毅 <383164014@qq.com>
2021-04-09 18:54:22 +0800

    新增文件大小

--- helper/file.go
+++ helper/file.go
@@ -108,3 +108,20 @@ func CreateDirIfNotExists(path string) error {
 	}
 	return os.MkdirAll(path, os.ModePerm)
 }
+
+// FormatFileSizeAndUnit 格式化文件大小
+func FormatFileSizeAndUnit(fileSize int64) (string, string) {
+	if fileSize < 1024 {
+		return fmt.Sprintf("%.2f", float64(fileSize)/float64(1)), "B"
+	} else if fileSize < (1024 * 1024) {
+		return fmt.Sprintf("%.2f", float64(fileSize)/float64(1024)), "KB"
+	} else if fileSize < (1024 * 1024 * 1024) {
+		return fmt.Sprintf("%.2f", float64(fileSize)/float64(1024*1024)), "MB"
+	} else if fileSize < (1024 * 1024 * 1024 * 1024) {
+		return fmt.Sprintf("%.2f", float64(fileSize)/float64(1024*1024*1024)), "GB"
+	} else if fileSize < (1024 * 1024 * 1024 * 1024 * 1024) {
+		return fmt.Sprintf("%.2f", float64(fileSize)/float64(1024*1024*1024*1024)), "TB"
+	} else {
+		return fmt.Sprintf("%.2f", float64(fileSize)/float64(1024*1024*1024*1024*1024)), "EB"
+	}
+}
