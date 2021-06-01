84d409528b701faec0d8f7bd9005515e6c49db4b
许弘毅 <383164014@qq.com>
2021-06-01 22:01:54 +0800

    删除调试代码

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/xhyonline/xutil/sig"
-)
-
-type c struct {
-}
-
-func (s *c) GracefulClose() {
-
-	fmt.Println("测试关闭")
-	time.Sleep(time.Second * 10)
-	fmt.Println("关闭结束")
-}
-
 func main() {
-	s := new(c)
-	closed := sig.RegisterOnClose(s)
-	<-closed
+
 }
